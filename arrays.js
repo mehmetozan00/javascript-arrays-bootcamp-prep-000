8fcee5c51081209544fa947cb44affe571c19aa1
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-11 15:16:09 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -42,6 +42,5 @@ function destructivelyRemoveElementFromEndOfArray (chocolateBars){
 
 ////////////////////////
 function removeElementFromEndOfArray (chocolateBars){
-  chocolateBars.slice(0, chocolateBars.length-1);
-  return chocolateBars;
+  return chocolateBars.slice(0, chocolateBars.length-1);
 }
